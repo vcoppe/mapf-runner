b7eef36dc433fd0ff3385d2355a8c6aa308d5a2b
vcoppe <vianney.coppe@gmail.com>
2021-10-19 11:19:53 +0200

    update scripts and add exectutables and config files

--- run.sh
+++ run.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# get this directory
-cur_dir=$(pwd)
-script="$cur_dir/run_instance.sh"
-
 # move to CCBS directory
-cd $1
+# cd $1
 
 # update
-git pull
+# git pull
 
 # recompile
-cmake .
-make
+# cmake .
+# make
 
 # get number of processors
 n=$(nproc --all)
 
 # get all original instance files
-find Instances -name all -prune -o -name \*.xml -not -name \*map.xml -print | parallel -P $n --bar $script {}
+find "$1/Instances" -name all -prune -o -name \*.xml -not -name \*map.xml -print | parallel -P $n --bar ./run_instance.sh $2 {}
--- run_instance.sh
+++ run_instance.sh
@@ -3,7 +3,8 @@
 xml_start="<?xml version=\"1.0\" ?><root>"
 xml_end="</root>"
 
-instance=$1
+executable=$1
+instance=$2
 
 # find corresponding map file (map.xml in same folder)
 dir=$(dirname $instance)
@@ -13,19 +14,43 @@ file=$(basename $instance | cut -f 1 -d '.')
 mkdir -p "$dir/all"
 # find max number of agents
 n=$(grep "agent" $instance -c)
-# for each number of agents
-for ((k=2; k<=$n; k++)) do
-    input_file="$dir/all/$file-$k.xml"
-    output_file="$dir/all/$file-$k.out"
-    # create file with the k first agents
-    echo $xml_start > $input_file
-    grep -m$k "agent" $instance >> $input_file
-    echo $xml_end >> $input_file
-    # run CCBS on the instance
-    ./CCBS $map $input_file > $output_file
-    # if no solution found, break
-    sol=$(grep "Soulution found: true" $output_file -c)
-    if [[ sol -eq 0 ]]; then
-        break
-    fi
-done
+
+if [[ $dir == *"roadmaps"* ]]; then
+    # for each number of agents
+    for ((k=2; k<=$n; k++)) do
+        input_file="$dir/all/$file-$k.xml"
+        output_file="$dir/all/$file-$k.out"
+        # create file with the k first agents
+        echo $xml_start > $input_file
+        grep -m$k "agent" $instance >> $input_file
+        echo $xml_end >> $input_file
+        # run CCBS on the instance
+        "./$executable" $map $input_file $config3 > $output_file
+        # if no solution found, break
+        sol=$(grep "Soulution found: true" $output_file -c)
+        if [[ sol -eq 0 ]]; then
+            break
+        fi
+    done
+else
+    # for each connectedness
+    for ((c=2; c<=5; c++)) do
+        config="config-$c.xml"
+        # for each number of agents
+        for ((k=2; k<=$n; k++)) do
+            input_file="$dir/all/$file-$k-c$c.xml"
+            output_file="$dir/all/$file-$k-c$c.out"
+            # create file with the k first agents
+            echo $xml_start > $input_file
+            grep -m$k "agent" $instance >> $input_file
+            echo $xml_end >> $input_file
+            # run CCBS on the instance
+            "./$executable" $map $input_file $config > $output_file
+            # if no solution found, break
+            sol=$(grep "Soulution found: true" $output_file -c)
+            if [[ sol -eq 0 ]]; then
+                break
+            fi
+        done
+    done
+fi
