e811a53614b51f5efe1636acafb0833a443099a1
vcoppe <vianney.coppe@gmail.com>
2021-10-20 13:06:13 +0200

    update

--- launch.sh
+++ launch.sh
@@ -13,11 +13,11 @@ dir=$(pwd)
 #
 # ./run.sh ../Continuous-CBS
 # ./extract.sh ../Continuous-CBS ../results/original
-#
-# cd ../Continuous-CBS
-#
-# rm CMakeCache.txt
-# rm -r CMakeFiles
+
+cd ../Continuous-CBS
+
+rm CMakeCache.txt
+rm -r CMakeFiles
 
 git checkout dev
 git pull
