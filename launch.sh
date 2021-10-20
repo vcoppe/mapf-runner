673863b5dd551232c88d2fd7f9cd77c0fbedeca0
vcoppe <vianney.coppe@gmail.com>
2021-10-20 13:05:03 +0200

    test only with dev

--- launch.sh
+++ launch.sh
@@ -2,24 +2,22 @@
 
 dir=$(pwd)
 
-git pull
-
-cd ../Continuous-CBS
-
-git checkout master
-git pull
-cmake .
-make
-
-cd $dir
-
-./run.sh ../Continuous-CBS
-./extract.sh ../Continuous-CBS ../results/original
-
-cd ../Continuous-CBS
-
-rm CMakeCache.txt
-rm -r CMakeFiles
+# cd ../Continuous-CBS
+#
+# git checkout master
+# git pull
+# cmake .
+# make
+#
+# cd $dir
+#
+# ./run.sh ../Continuous-CBS
+# ./extract.sh ../Continuous-CBS ../results/original
+#
+# cd ../Continuous-CBS
+#
+# rm CMakeCache.txt
+# rm -r CMakeFiles
 
 git checkout dev
 git pull
